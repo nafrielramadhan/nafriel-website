b9c532a0eb57f8b4bb91299a683a29d2ceebb281
nafrielramadhan <muhammad.nafriel@gmail.com>
2025-05-14 22:57:25 +0700

    Initial commit

---
+++ src/app/contact/page.js
@@ -0,0 +1,32 @@
+export default function ContactPage() {
+  return (
+    <>
+      <nav>
+        <div className="name">
+          Contact <a href="/">NafrielRamadhan.</a>
+        </div>
+        <div className="navbar-button">
+          <a href="/about">ABOUT</a>
+          <a href="/work">WORK</a>
+          <a href="/contact">CONTACT</a>
+        </div>
+      </nav>
+
+      <div className="container">
+        <main>
+          <div className="progress">
+            <h1>
+              This page is still on <br />
+              <gradient-underline>progress</gradient-underline>.
+            </h1>
+          </div>
+        </main>
+      </div>
+
+      {/* gradient ball */}
+      <div className="absolute">
+        <div className="gradient-circle"></div>
+      </div>
+    </>
+  );
+}
